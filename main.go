5410aa609e0485c140ec0eff593ad0d767049c47
hgedia <hgedia@hgedia.co>
2019-09-30 17:57:56 +0100

    Dependency tree

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main(){
-	fmt.Println("Hello")
+	"github.com/casbin/casbin"
+)
+
+func main() {
+	e, _ := casbin.NewEnforcer("model.conf", "policy.csv")
+	//admin
+	e.AddPermissionForUser("admin", "ad_campaign", "GET")
+	e.AddPermissionForUser("admin", "ad_campaign", "LIST")
+	//area-admin
+	e.AddPermissionForUser("area_ad_admin", "campaign", "WRITE")
+	e.AddPermissionForUser("area_ad_admin", "adgroup", "WRITE")
+	e.AddPermissionForUser("area_ad_admin", "adcreative", "WRITE")
+	//assigns role of area_ad_admin to admin.
+	e.AddRoleForUser("admin", "area_ad_admin")
+
+	//end users.
+	e.AddRoleForUser("kevin", "admin")
+	e.SavePolicy()
+
+	//print.
+	result, _ := e.GetImplicitPermissionsForUser("kevin")
+	fmt.Printf("%#v", result)
 }
-
