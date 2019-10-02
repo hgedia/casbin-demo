32951331055dc9f9d4b61f2b0965afb421a009c8
hgedia <hgedia@hgedia.com>
2019-10-02 23:10:01 +0100

    Trial and error

--- main.go
+++ main.go
@@ -6,11 +6,24 @@ import (
 	"github.com/casbin/casbin"
 )
 
+/*
+User story
+ - Users in system User01, User02
+ - Group 1 : InvoiceNo, InvoiceDate,ManufactuererID, Origin,MineName
+ - Group 2 : Origin, KPNumber, Weight
+
+1. As manufacturer : manu-id-01 , I want to give access group_1 perm to User01
+2. As manufacturer: manu-did-02, I want to give access group_2 perm to User02
+
+3. As manufacturer: manu-id-01 , I want to give access to group_2 only for oid "object-id-01" to "User01"
+4. As manufacturer: manu-id-01 , I want to give access to group 1
+*/
+
 func printUserPermissions(e *casbin.Enforcer, usr string) {
-	fmt.Println("Persmissions for user : ", usr)
+	fmt.Println("Permissions for user : ", usr)
 	result, _ := e.GetImplicitPermissionsForUser(usr)
 	for _, v := range result {
-		fmt.Printf("%s:%s\n", v[0], v[1])
+		fmt.Printf("%s\n", v)
 	}
 	fmt.Println("=============================")
 }
@@ -22,35 +35,28 @@ func main() {
 	//UID 200 -!OBI 100
 
 	//Group 1 Permission Manufacturers
-	e.AddPermissionForUser("group_1", "InvoiceNo", "read")
-	e.AddPermissionForUser("group_1", "InvoiceDate", "read")
-	e.AddPermissionForUser("group_1", "ManufacturerId", "read")
-	e.AddPermissionForUser("group_1", "Origin", "read")
-	e.AddPermissionForUser("group_1", "MineName", "read")
+	e.AddPermissionForUser("role:manu-id-01_group", "InvoiceNo")
+	e.AddPermissionForUser("role:manu-id-01_group", "InvoiceDate")
+	e.AddPermissionForUser("role:manu-id-01_group", "Origin")
 
-	//Group 2 Permissions Retailers
-	e.AddPermissionForUser("group_2", "Origin", "read")
-	e.AddPermissionForUser("group_2", "KPNumber", "read")
-	e.AddPermissionForUser("group_2", "Weight", "read")
+	//Group 2 Permission Manufacturers
+	e.AddPermissionForUser("role:manu-id-02_group_1", "InvoiceNo")
+	e.AddPermissionForUser("role:manu-id-02_group_1", "InvoiceDate")
+	e.AddPermissionForUser("role:manu-id-02_group_1", "Origin")
 
-	//Admin gets all permisssions
-	e.AddRoleForUser("admin", "group_1")
-	e.AddRoleForUser("admin", "group_2")
-
-	e.AddRoleForUser("uuid-100-204-100", "group_1")
+	//Group 3 Permission Manufacturer no 2
+	e.AddPermissionForUser("role:manu-id-02_group_2", "Origin")
+	e.AddPermissionForUser("role:manu-id-02_group_2", "KPNumber")
+	e.AddPermissionForUser("role:manu-id-02_group_2", "Weight")
 
 	//Haresh gets only group 1
-	e.AddRoleForUser("haresh", "group_1")
-	e.AddRoleForUser("priyav", "group_2")
-	e.AddRoleForUser("david", "admin")
+	e.AddRoleForUser("haresh", "role:manu-id-02_group_2")
+	e.AddRoleForUser("haresh", "role:manu-id-01_group")
+
+	e.AddPermissionForUser("haresh", "dia", "oid", "muid", "Origin", "deny")
+
 	e.SavePolicy()
 
 	//print all permissions
 	printUserPermissions(e, "haresh")
-	printUserPermissions(e, "priyav")
-	printUserPermissions(e, "david")
-
-	//Check if user has permission
-	fmt.Println(e.HasPermissionForUser("haresh", "InvoiceNo", "read"))
-	fmt.Println(e.HasPermissionForUser("group_1", "InvoiceNo", "read"))
 }
